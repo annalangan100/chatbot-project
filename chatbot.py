2f9da83efe7d1aa3d80e18024c43865d52b9ff21
Anna Langan <annalangan100@gmail.com>
2026-06-05 09:52:27 +0100

    Added basic chatbot responses

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,21 @@
 print("Chatbot started!")
+print("Type 'bye' to exit.")
 
 while True:
-    user_input = input("You: ")
+    user_input = input("You: ").lower()
 
-    if user_input.lower() == "bye":
+    if user_input == "bye":
         print("Bot: Goodbye!")
         break
 
-    print("Bot: You said:", user_input)
+    elif user_input == "hello":
+        print("Bot: Hello!")
+
+    elif user_input == "how are you":
+        print("Bot: I'm doing well, thanks for asking!")
+
+    elif user_input == "what is your name":
+        print("Bot: My name is ChatBot.")
+
+    else:
+        print("Bot: I don't understand that yet.")

b46840dda7f2fd06da109209a9244a22829ea87c
Anna Langan <annalangan100@gmail.com>
2026-06-06 09:57:46 +0100

    Added memory support to Flask interface

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template, request
 import json
 
 from utils.intent_handler import get_response
+from utils.memory import load_user_data
+from utils.memory_commands import handle_memory_command
 
 app = Flask(__name__)
 
 with open("intents.json", "r") as file:
     intents = json.load(file)
+
+user_data = load_user_data()
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -17,10 +21,21 @@ def home():
 
         user_input = request.form["message"].lower()
 
-        response = get_response(
+        memory_response = handle_memory_command(
             user_input,
-            intents
+            user_data
         )
+
+        if memory_response:
+
+            response = memory_response
+
+        else:
+
+            response = get_response(
+                user_input,
+                intents
+            )
 
     return render_template(
         "index.html",

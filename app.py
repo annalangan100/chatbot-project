06bffa6b1e8fc4c0b596fc5761d94ed45d7615b2
Anna Langan <annalangan100@gmail.com>
2026-06-06 09:53:22 +0100

    Connected chatbot responses to Flask interface

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
+
+from utils.intent_handler import get_response
 
 app = Flask(__name__)
 
+with open("intents.json", "r") as file:
+    intents = json.load(file)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+
+    response = ""
+
+    if request.method == "POST":
+
+        user_input = request.form["message"].lower()
+
+        response = get_response(
+            user_input,
+            intents
+        )
+
+    return render_template(
+        "index.html",
+        response=response
+    )
 
 
 if __name__ == "__main__":
